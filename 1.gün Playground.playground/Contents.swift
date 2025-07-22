0e83e306b5daff9f94752dbace166047ccd98daf
Aymina Çakır <114817398+ayminacakir@users.noreply.github.com>
2025-07-22 11:02:40 +0300

    1.gün Xcode Playground dosyası eklendi

---
+++ 1.gün Playground.playground/Contents.swift
@@ -0,0 +1,44 @@
+import UIKit
+//Variables
+var str = "Hello,playground"
+
+var age = 38
+
+var favoriteShow = "Orange is the New Black"
+favoriteShow = "The Good Place"
+favoriteShow = "Doctor Who"
+
+//Değişkenler, programımızda geçici bilgiler depolamamızı sağlar,değiştirilebilir.
+
+//Multi-line strings
+var str1 = """
+This goes
+over multiple 
+lines
+"""
+
+var str2 = """
+    This goes \
+    over multiple \
+    lines
+    """
+
+//Doubles and Booleans
+var pi = 3.141
+var awesome: Bool = true
+
+
+//String interpolation
+
+var score = 85
+var str = "Your score was \(score)"
+var results = "The test results are here: \(str)"
+
+//Type annotations
+
+let album: String = "Reputation"
+let year: Int = 1989
+let heigh: Double = 1.78
+let weight: Double = 65
+var percentage: Double = 99
+var name: String
